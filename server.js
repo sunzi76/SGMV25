b58451aa4ae30e2c8b0c108d6919203cd3b2dc97
Datim76 <tua_email@example.com>
2025-08-02 00:01:52 +0200

    Fix SyntaxError: Identifier 'multer' has already been declared

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const cors = require('cors');
 const archiver = require('archiver');
 /* AWS Costanti*/
 const AWS = require('aws-sdk');
-const multer = require('multer');
 const multerS3 = require('multer-s3');
 
 // Configura AWS SDK
